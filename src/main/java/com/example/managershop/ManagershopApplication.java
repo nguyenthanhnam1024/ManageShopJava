0053b0b9239b0f58aeebf828163b5930f4ca896b
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-04-08 13:54:14 +0700

    added files of project manager shop initial

---
+++ src/main/java/com/example/managershop/ManagershopApplication.java
@@ -0,0 +1,13 @@
+package com.example.managershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagershopApplication.class, args);
+	}
+
+}
