2d48762ffa6678a9753fef6f8d67ff5a0b62f8ec
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-04-12 15:21:32 +0700

    user @SuppressWarning for entity IdRoleUser

--- src/main/java/com/example/managershop/entity/IdRoleUser.java
+++ src/main/java/com/example/managershop/entity/IdRoleUser.java
@@ -2,5 +2,8 @@ package com.example.managershop.entity;
 
 import java.io.Serializable;
 
+@SuppressWarnings("unused")
 public class IdRoleUser implements Serializable {
+    private Long idUser;
+    private Long idRole;
 }
