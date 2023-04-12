e76c125e8c5cafc7efcd2f7d878cd13a7aa0b2d1
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-04-12 16:28:58 +0700

    create feature getAll, save, update, delete cho Account

---
+++ src/main/java/com/example/managershop/controller/AccountController.java
@@ -0,0 +1,38 @@
+package com.example.managershop.controller;
+
+import com.example.managershop.entity.Account;
+import com.example.managershop.service.AccountService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/getAll")
+    public List<Account> getAllAccount() {
+        return accountService.getAllAccount();
+    }
+
+    @PostMapping("save")
+    public void saveAccount(@Valid @RequestBody Account account) {
+        accountService.saveAccount(account);
+    }
+
+    @PutMapping("/update")
+    public void updateAccount(@Valid @RequestBody Account account) {
+        accountService.updateAccount(account);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+    }
+}
---
+++ src/main/java/com/example/managershop/service_ipm/AccountServiceIpm.java
@@ -0,0 +1,38 @@
+package com.example.managershop.service_ipm;
+
+import com.example.managershop.entity.Account;
+import com.example.managershop.repository.AccountRepo;
+import com.example.managershop.service.AccountService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountServiceIpm implements AccountService {
+    private final AccountRepo accountRepo;
+
+    public AccountServiceIpm(AccountRepo accountRepo) {
+        this.accountRepo = accountRepo;
+    }
+
+
+    @Override
+    public List<Account> getAllAccount() {
+        return accountRepo.findAll();
+    }
+
+    @Override
+    public void saveAccount(Account account) {
+        accountRepo.save(account);
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        accountRepo.save(account);
+    }
+
+    @Override
+    public void deleteAccount(Long id) {
+        accountRepo.deleteById(id);
+    }
+}
