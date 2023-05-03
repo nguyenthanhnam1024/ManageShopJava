06c05f049c4b5a22202cfe161624a4126e0d8e1f
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-05-03 15:34:59 +0700

    create

---
+++ src/main/java/com/example/manage_shops/config/UserDetailsImp.java
@@ -0,0 +1,5 @@
+package com.example.manage_shops.config;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImp implements UserDetails {
+}
---
+++ src/main/java/com/example/manage_shops/jwt/JwtUtils.java
@@ -0,0 +1,6 @@
+package com.example.manage_shops.jwt;
+
+public class JwtUtils {
+    private final String JWT_SECRET = "nguyenthanhnam";
+    private final Long JWT_EXPERATION = 3600000L;
+}
