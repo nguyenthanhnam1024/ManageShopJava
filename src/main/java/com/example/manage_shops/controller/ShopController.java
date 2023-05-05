72ce8de165ebddfa9d57044a042992e5e29c7d6c
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-05-05 17:42:02 +0700

    complete feature shop

--- src/main/java/com/example/manage_shops/controller/ShopController.java
+++ src/main/java/com/example/manage_shops/controller/ShopController.java
@@ -21,19 +21,16 @@ public class ShopController {
         this.commons = commons;
     }
 
-    @CrossOrigin(origins = "http://localhost:8080", maxAge = 36000000L)
     @GetMapping("/getAll/{roleIdUser}")
     public ResponseEntity<?> getAllShop(@PathVariable int roleIdUser) throws MyValidateException {
         return ResponseEntity.ok(shopService.getAllShop(roleIdUser));
     }
 
-    @CrossOrigin(origins = "http://localhost:8080", maxAge = 36000000L)
     @GetMapping("/getById/{shopId}/{roleIdUser}")
     public ResponseEntity<?> getShopById(@PathVariable int shopId, @PathVariable int roleIdUser) throws MyValidateException {
         return ResponseEntity.ok(shopService.getShopById(shopId, roleIdUser));
     }
 
-    @CrossOrigin(origins = "http://localhost:8080", maxAge = 36000000L)
     @PostMapping("/save/{roleIdUser}")
     public ResponseEntity<?> saveShop(@Valid @RequestBody Shop shop, @PathVariable int roleIdUser, BindingResult result) throws MyValidateException {
         if (result.hasErrors()) {
@@ -42,7 +39,6 @@ public class ShopController {
         return ResponseEntity.ok(shopService.saveShop(shop, roleIdUser));
     }
 
-    @CrossOrigin(origins = "http://localhost:8080", maxAge = 36000000L)
     @PutMapping("/update/{roleIdUser}")
     public ResponseEntity<?> updateShop(@Valid @RequestBody Shop shop, @PathVariable int roleIdUser, BindingResult result) throws MyValidateException {
         if (result.hasErrors()) {
@@ -51,13 +47,11 @@ public class ShopController {
         return ResponseEntity.ok(shopService.updateShop(shop, roleIdUser));
     }
 
-    @CrossOrigin(origins = "http://localhost:8080", maxAge = 36000000L)
     @DeleteMapping("/delete/{shopId}/{roleIdUser}")
     public ResponseEntity<?> deleteRole(@PathVariable int shopId, @PathVariable int roleIdUser) throws MyValidateException{
         return ResponseEntity.ok(shopService.deleteShop(shopId, roleIdUser));
     }
 
-    @CrossOrigin(origins = "http://localhost:8080", maxAge = 36000000L)
     @GetMapping("/getByKeyword/{roleIdUser}")
     public ResponseEntity<?> getShopByKeyword(@PathVariable int roleIdUser, @RequestParam String keyword) throws MyValidateException {
         return ResponseEntity.ok(shopService.getShopByKeyword(keyword, roleIdUser));
