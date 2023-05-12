164566123f9f8cae26f6fa7e10e7226882160db0
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-05-12 18:45:59 +0700

    reset

--- src/main/java/com/example/manage_shops/repository/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.manage_shops.repository;
-
-import com.example.manage_shops.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepo extends JpaRepository<Role, Integer> {
-}
