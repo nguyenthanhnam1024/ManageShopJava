5580b949ece01495f6da569c91cdd7669ec16164
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-05-29 15:27:55 +0700

    create function save for role

--- src/main/java/com/example/manage_shops/controller/RoleController.java
+++ src/main/java/com/example/manage_shops/controller/RoleController.java
@@ -1,14 +1,12 @@
 package com.example.manage_shops.controller;
 
 import com.example.manage_shops.exception.MyValidateException;
+import com.example.manage_shops.request.RequestRole;
 import com.example.manage_shops.response.ResponseLogin;
 import com.example.manage_shops.service.RoleService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/role")
@@ -19,5 +17,11 @@ public class RoleController {
     @GetMapping("getAll")
     public ResponseEntity<?> getAllRole(@RequestBody ResponseLogin responseLogin) throws MyValidateException {
         return ResponseEntity.ok(roleService.getAllRole(responseLogin));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveRole(@RequestBody RequestRole requestRole) throws MyValidateException {
+        roleService.saveRole(requestRole);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/manage_shops/service_ipm/RoleServiceImp.java
+++ src/main/java/com/example/manage_shops/service_ipm/RoleServiceImp.java
@@ -3,13 +3,16 @@ package com.example.manage_shops.service_ipm;
 import com.example.manage_shops.entity.Role;
 import com.example.manage_shops.exception.MyValidateException;
 import com.example.manage_shops.repository.RoleRepo;
+import com.example.manage_shops.request.RequestRole;
 import com.example.manage_shops.response.ResponseLogin;
 import com.example.manage_shops.service.Commons;
 import com.example.manage_shops.service.RoleService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -25,5 +28,22 @@ public class RoleServiceImp implements RoleService {
         } catch (Exception ex) {
             throw new MyValidateException("get list role failure");
         }
+    }
+
+    @Override
+    @Transactional
+    public void saveRole(RequestRole requestRole) throws MyValidateException {
+        commons.validateRoleForADMIN(requestRole.getResponseLogin().getRole());
+        Optional<Role> roleOptional = roleRepo.findByRoleName(requestRole.getRoleName());
+        if (!roleOptional.isPresent()) {
+            try {
+                Role newRole = new Role();
+                newRole.setRoleName(requestRole.getRoleName());
+                roleRepo.save(newRole);
+            } catch (Exception ex) {
+                throw new MyValidateException("save role failure");
+            }
+        }
+        throw new MyValidateException("Role name have been exist");
     }
 }
