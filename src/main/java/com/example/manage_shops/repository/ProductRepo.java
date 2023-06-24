edc15d8acd0106d7430b0c72fbaf383185c74e56
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-06-24 20:28:11 +0700

    project fine-turning

--- src/main/java/com/example/manage_shops/repository/ProductRepo.java
+++ src/main/java/com/example/manage_shops/repository/ProductRepo.java
@@ -5,12 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
     List<Product> findProductByIdShop(int idShop);
-
     Product findProductByName(String productName);
-
     List<Product> findByNameContainingIgnoreCase(String keyword);
+    Optional<Product> findByName(String productName);
 }
